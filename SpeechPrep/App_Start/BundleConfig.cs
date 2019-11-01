e07ed7129b63682f8b1f6dbe02119ed686932796
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-10-31 23:26:40 -0700

    Updates to Bootstrap/Jquery

--- SpeechPrep/App_Start/BundleConfig.cs
+++ SpeechPrep/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SpeechPrep
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-superhero.css",
                       "~/Content/site.css"));
         }
     }
