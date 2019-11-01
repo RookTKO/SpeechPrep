771386fa34868a3197905472d3f6886a3b936644
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-10-31 22:53:52 -0700

    Implemented question controller and added question model and random question page.

---
+++ SpeechPrep/Controllers/QuestionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SpeechPrep.Models;
+
+namespace SpeechPrep.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question/Random
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Random()
+        {
+            var question = new Question() { InterviewQuestion = "How many pickles did peter pick on the pickling tree?" };
+            return View(question);
+        }
+    }
+}
---
+++ SpeechPrep/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpeechPrep.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string InterviewQuestion { get; set; }
+        public string StarType { get; set; }
+    }
+}
