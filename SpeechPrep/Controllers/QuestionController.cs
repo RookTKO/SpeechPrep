a8dd0abef2e2e6f29968b85f9306fc4af9cee2a9
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-11-01 18:20:48 -0700

    Added questions page

--- SpeechPrep/Controllers/QuestionController.cs
+++ SpeechPrep/Controllers/QuestionController.cs
@@ -19,5 +19,11 @@ namespace SpeechPrep.Controllers
             var question = new Question() { InterviewQuestion = "How many pickles did peter pick on the pickling tree?" };
             return View(question);
         }
+        
+        public ActionResult Questions()
+        {
+            var question = new Question() { InterviewQuestion = "How many pickles did peter pick on the pickling tree?" };
+            return View(question);
+        }
     }
 }
