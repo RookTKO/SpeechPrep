67fa4d769638ae13b7772134b5303533765057b6
RookTKO <olaf.a.minkowicz@gmail.com>
2019-11-02 23:20:00 -0700

    Updating layout.cshtml to bootstrap 4

--- SpeechPrep/App_Start/BundleConfig.cs
+++ SpeechPrep/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SpeechPrep
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-superhero.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
