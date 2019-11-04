d3099450cf2cc47915d3db1d73a2c848b9fbe23b
RookTKO <olaf.a.minkowicz@gmail.com>
2019-11-04 14:53:23 -0800

    Started DbContext creation and migration.

---
+++ SpeechPrep/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpeechPrep.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- SpeechPrep/Models/User.cs
+++ SpeechPrep/Models/User.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace SpeechPrep.Models
 {
-    public class User
+    public class User : IdentityUser
     {
     }
 }
