2c8c55422081b0ed6765cc79f5ba85db1ddfc4b1
RookTKO <olaf.a.minkowicz@gmail.com>
2019-11-04 22:43:21 -0800

    Updated questions model

---
+++ SpeechPrep/Migrations/201911050638255_UpdateWithApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace SpeechPrep.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateWithApplicationDbContext : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- SpeechPrep/Models/Question.cs
+++ SpeechPrep/Models/Question.cs
@@ -10,6 +10,9 @@ namespace SpeechPrep.Models
         public int Id { get; set; }
         public string InterviewQuestion { get; set; }
         public string StarType { get; set; }
+        public bool Favorite { get; set; }
+        public bool SavedForLater { get; set; }
+        public bool Completed { get; set; }
 
         //TODO: Either use enums for question state or use methods.
         //public static bool IsFavoritedQuestion()
