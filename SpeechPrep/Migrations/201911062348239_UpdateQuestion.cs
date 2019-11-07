893757fdbaf6fbe61733805021e52108bb7e8dd2
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-11-06 16:10:05 -0800

    Updated Questions

---
+++ SpeechPrep/Migrations/201911062348239_UpdateQuestion.cs
@@ -0,0 +1,20 @@
+﻿namespace SpeechPrep.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateQuestion : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT Questions ON");
+            Sql("INSERT INTO Questions (Id, InterviewQuestion, StarType) VALUES (1, 'Tell me about yourself.', 'Null')");
+            Sql("INSERT INTO Questions (Id, InterviewQuestion, StarType) VALUES (2, 'How do you handle stress and pressure?', 'Null')");
+            Sql("SET IDENTITY_INSERT Questions OFF");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
