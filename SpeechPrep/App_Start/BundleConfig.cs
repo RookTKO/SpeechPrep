6107a99bd3daaf3c288540b7257b4b6c0f47d5bc
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-11-06 19:42:12 -0800

    Updated with new bundle and js file that will do DOM manipulations for layout views

--- SpeechPrep/App_Start/BundleConfig.cs
+++ SpeechPrep/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace SpeechPrep
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/customscript").Include(
+                        "~/Scripts/juicying.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
