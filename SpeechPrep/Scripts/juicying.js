b7670afcda4d246dd680f590ef35b9f7fd85c042
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-11-14 13:24:44 -0800

    Updated jumbotron with particle.js

--- SpeechPrep/Scripts/juicying.js
+++ SpeechPrep/Scripts/juicying.js
@@ -9,12 +9,60 @@ window.addEventListener("scroll", () => {
         navBar.setAttribute("id", "navBarShadowOut");
     }
 })
-
 window.onload = function () {
     console.log("working");
     Particles.init({
         selector: '.contextCanvasBG',
-        color: '#DA0463'
-    });
-};
 
+        responsive: [
+            {
+                breakpoint: 2560,
+                options: {
+                    maxParticles: 100,
+                    sizeVariations: 5,
+                    speed: 0.66,
+                    color: ['#48F2E3', '#404B69', '#48F2E3'],
+                    connectParticles: true
+                }
+            }, {
+                breakpoint: 1440,
+                options: {
+                    maxParticles: 100,
+                    sizeVariations: 3,
+                    speed: 0.66,
+                    color: ['#48F2E3', '#404B69', '#48F2E3'],
+                    connectParticles: true
+                }
+            }, {
+                breakpoint: 1024,
+                options: {
+                    maxParticles: 100,
+                    sizeVariations: 3,
+                    speed: 0.66,
+                    color: ['#48F2E3', '#404B69', '#48F2E3'],
+                    connectParticles: true
+                }
+            },{
+                breakpoint:768,
+                options: {
+                    maxParticles: 100,
+                    sizeVariations: 3,
+                    speed: 0.66,
+                    color: ['#48F2E3', '#404B69', '#48F2E3'],
+                    connectParticles: true
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 0
+                }
+            }, {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 0
+                }
+            }
+        ]
+    });
+
+};
