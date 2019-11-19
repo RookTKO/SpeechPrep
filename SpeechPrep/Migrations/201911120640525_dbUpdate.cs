855f0b5d9e92f9dfe5d4e081af85b28054c2f134
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2019-11-19 14:34:32 -0800

    Fixed migration issues.

--- SpeechPrep/Migrations/201911120640525_dbUpdate.cs
+++ SpeechPrep/Migrations/201911120640525_dbUpdate.cs
@@ -8,8 +8,8 @@
         public override void Up()
         {
             Sql("SET IDENTITY_INSERT Questions ON");
-            Sql("INSERT INTO Questions (Id, InterviewQuestion, StarType) VALUES (1, 'Tell me about yourself.', 'Null')");
-            Sql("INSERT INTO Questions (Id, InterviewQuestion, StarType) VALUES (2, 'How do you handle stress and pressure?', 'Null')");
+            //Sql("INSERT INTO Questions (Id, InterviewQuestion, StarType) VALUES (1, 'Tell me about yourself.', 'Null')");
+            //Sql("INSERT INTO Questions (Id, InterviewQuestion, StarType) VALUES (2, 'How do you handle stress and pressure?', 'Null')");
             Sql("SET IDENTITY_INSERT Questions OFF");
         }
         public override void Down()
