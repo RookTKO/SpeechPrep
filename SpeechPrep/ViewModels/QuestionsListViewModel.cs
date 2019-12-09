7e02d28234cdcb679f636b6cf7ce1196ff08e34e
RookTKO <olaf.a.minkowicz@gmail.com>
2019-12-09 15:42:44 -0800

    Updated with comments for future todos

--- SpeechPrep/ViewModels/QuestionsListViewModel.cs
+++ SpeechPrep/ViewModels/QuestionsListViewModel.cs
@@ -9,5 +9,7 @@ namespace SpeechPrep.ViewModels
     public class QuestionsListViewModel
     {
         public IEnumerable<Question> QuestionList { get; set; }
+
+        //TODO : Add profile model into modelview so users don't have issue logining in.
     }
 }
